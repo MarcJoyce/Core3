f0bba584b0ec3b69e6c979bf6d1004139815c850
Marc Joyce <marcjoyce90@gmail.com>
2024-05-19 21:28:55 +0100

    Change force cost on saber and lightsaber to be decimal

--- MMOCoreORB/src/server/zone/objects/tangible/weapon/WeaponObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/weapon/WeaponObjectImplementation.cpp
@@ -353,7 +353,7 @@ void WeaponObjectImplementation::fillAttributeList(AttributeListMessage* alm, Cr
 
 	// Force Cost
 	if (getForceCost() > 0)
-		alm->insertAttribute("forcecost", (int)getForceCost());
+		alm->insertAttribute("forcecost", Math::getPrecision(getForceCost(), 2));
 
 	for (int i = 0; i < getNumberOfDots(); i++) {
 
