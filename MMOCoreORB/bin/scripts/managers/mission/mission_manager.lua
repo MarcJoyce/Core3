95b0f0d43d650964971d7883d339eea843727caa
MarcJoyce <MarcJoyce90@googlemail.com>
2024-07-02 22:03:52 +0100

    Fix DJM loot to be higher level Amend loot_manager stats for better loot chance Reduce mission distance

--- MMOCoreORB/bin/scripts/managers/mission/mission_manager.lua
+++ MMOCoreORB/bin/scripts/managers/mission/mission_manager.lua
@@ -82,17 +82,17 @@ enable_factional_entertainer_missions = "true"
 
 enable_same_account_bounty_missions = "false"
 
-playerBountyKillBuffer = 30 * 60 * 1000 -- Buffer before player bounty can be put back on terminal after target is killed, set 0 to disable
+playerBountyKillBuffer = 5 * 60 * 1000 -- Buffer before player bounty can be put back on terminal after target is killed, set 0 to disable
 
-playerBountyDebuffLength = 3 * 24 * 60 * 60 * 1000 -- Time before their bounty resets from the minimum amount
+playerBountyDebuffLength = 5 * 60 * 1000 -- Time before their bounty resets from the minimum amount
 
 -- Destroy Mission Configuration
 -- Distance calculated as: 
 --    <BaseDistance> + <DifficultyDistanceFactor> * <difficultyLevel> + 
 --    rand(<RandomDistance>) + rand(<DifficutlyRandomDistance * <difficultyLevel>)
-destroyMissionBaseDistance = 1000
+destroyMissionBaseDistance = 750
 destroyMissionDifficultyDistanceFactor = 0
-destroyMissionRandomDistance = 1000
+destroyMissionRandomDistance = 750
 destroyMissionDifficultyRandomDistance = 0
 
 -- Mission payout calculated as: 
